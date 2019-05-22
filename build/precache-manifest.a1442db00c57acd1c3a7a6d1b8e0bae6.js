88d86c953ec1b5ef1e8bf03a3fee0b22416fa99d
matt-writes-code <37174946+mayojich@users.noreply.github.com>
2019-05-22 21:02:57 +0800

    npm run build

---
+++ build/precache-manifest.a1442db00c57acd1c3a7a6d1b8e0bae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bdff80b11af969effdaa",
+    "url": "/static/js/main.c13e69d0.chunk.js"
+  },
+  {
+    "revision": "69306a0f649b639689b2",
+    "url": "/static/js/2.e6954938.chunk.js"
+  },
+  {
+    "revision": "bdff80b11af969effdaa",
+    "url": "/static/css/main.dabc5d22.chunk.css"
+  },
+  {
+    "revision": "69306a0f649b639689b2",
+    "url": "/static/css/2.27b0db02.chunk.css"
+  },
+  {
+    "revision": "38e077052891288bd0d081ddf6557b13",
+    "url": "/index.html"
+  }
+];
