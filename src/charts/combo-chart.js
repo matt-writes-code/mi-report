4184ac1c707406653b2312ee4acc93c11ffbc40a
matt-writes-code <37174946+mayojich@users.noreply.github.com>
2019-06-09 11:14:56 +0800

    fetch ready

--- src/charts/combo-chart.js
+++ src/charts/combo-chart.js
@@ -1,45 +1,68 @@
 import React from "react";
 import Chart from "react-google-charts";
-import Spinner from '../components/spinner.js';
+import { Spinner } from 'reactstrap';
 
-const ComboChart = () => {
-  return (
-    <div>
-      <Chart
-        width={"100%"}
-        height={"350px"}
-        chartType="ComboChart"
-        loader={<div><Spinner /></div>}
-        data={[
-          ["Month", "CS", "TS", "CR", "CRA", "EA", "Threshold"],
-          ["January", 500, 938, 522, 998, 450, 614.6],
-          ["February", 400, 1120, 599, 1268, 288, 682],
-          ["March", 550, 1167, 587, 807, 397, 623],
-          ["April", 650, 1110, 615, 968, 215, 609.4],
-          ["May", 880, 691, 629, 1026, 366, 569.6]
-        ]}
-        options={{
-          title: "Volume of alerts",
-          vAxis: {
-            title: "No of Alerts",
-            textStyle: { color: "#f0f0f0" },
-            titleTextStyle: { color: "#f0f0f0" }
-          },
-          hAxis: {
-            title: "Month",
-            textStyle: { color: "#f0f0f0" },
-            titleTextStyle: { color: "#f0f0f0" }
-          },
-          seriesType: "bars",
-          series: { 5: { type: "line" } },
-          backgroundColor: "black",
-          titleTextStyle: { color: "white" },
-          legendTextStyle: { color: "white" }
-        }}
-        rootProps={{ "data-testid": "1" }}
-      />
-    </div>
-  );
-};
+const combochartdata = 'https://api.myjson.com/bins/17o5b5';
 
-export default ComboChart;
+const oridata = [
+  ["Month", "IM", "PM", "CM", "CRA", "TR", "Threshold"],
+  ["January", 500, 938, 522, 998, 450, 614.6],
+  ["February", 400, 1120, 599, 1268, 288, 682],
+  ["March", 550, 1167, 587, 807, 397, 623],
+  ["April", 650, 1110, 615, 968, 215, 609.4],
+  ["May", 880, 691, 629, 1026, 366, 569.6]
+]
+
+export default class ComboChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${combochartdata}`)
+      .then(d => d.json())
+      .then(result => {
+        this.setState({
+          data: result
+        })
+      })
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div>
+        <Chart
+          width={"100%"}
+          height={"350px"}
+          chartType="ComboChart"
+          loader={<div><Spinner color="warning" /></div>}
+          data={data}
+          options={{
+            title: "Volume of alerts",
+            vAxis: {
+              title: "No of Alerts",
+              textStyle: { color: "#f0f0f0" },
+              titleTextStyle: { color: "#f0f0f0" }
+            },
+            hAxis: {
+              title: "Month",
+              textStyle: { color: "#f0f0f0" },
+              titleTextStyle: { color: "#f0f0f0" }
+            },
+            seriesType: "bars",
+            series: { 5: { type: "line" } },
+            backgroundColor: "black",
+            titleTextStyle: { color: "white" },
+            legendTextStyle: { color: "white" }
+          }}
+          rootProps={{ "data-testid": "1" }}
+        />
+      </div>
+    );
+  }
+}
--- src/charts/pie-chart.js
+++ src/charts/pie-chart.js
@@ -1,36 +1,58 @@
 import React from "react";
 import Chart from "react-google-charts";
-import Spinner from '../components/spinner.js';
+import { Spinner } from 'reactstrap';
 
-const PieChart = () => {
-  return (
-    <div>
-      <Chart
-        width={"100%"}
-        height={"350px"}
-        chartType="PieChart"
-        loader={<div><Spinner /></div>}
-        data={[
-          ["Task", "Hours per Day"],
-          ["Alert closures", 11],
-          ["Reports", 2],
-          ["Trainings", 2],
-          ["Meetings", 2],
-          ["Exception handling", 7]
-        ]}
-        options={{
-          title: "Daily Activities",
-          // Just add this option
-          pieHole: 0.6,
-          backgroundColor: "black",
-          titleTextStyle: { color: "white" },
-          legendTextStyle: { color: "white" },
-          legend: { position: "top", maxLines: "5" }
-        }}
-        rootProps={{ "data-testid": "3" }}
-      />
-    </div>
-  );
-};
+const piechartdata = "https://api.myjson.com/bins/zc2u9";
+const oridata = [
+  ["Task", "Hours per Day"],
+  ["Alert closures", 11],
+  ["Reports", 2],
+  ["Trainings", 2],
+  ["Meetings", 2],
+  ["Exception handling", 7]
+];
 
-export default PieChart;
+export default class PieChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${piechartdata}`)
+      .then(d => d.json())
+      .then(result => {
+        this.setState({
+          data: result
+        })
+      })
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div>
+        <Chart
+          width={"100%"}
+          height={"350px"}
+          chartType="PieChart"
+          loader={<div><Spinner color="info" /></div>}
+          data={data}
+          options={{
+            title: "Daily Activities",
+            // Just add this option
+            pieHole: 0.6,
+            backgroundColor: "black",
+            titleTextStyle: { color: "white" },
+            legendTextStyle: { color: "white" },
+            legend: { position: "top", maxLines: "5" }
+          }}
+          rootProps={{ "data-testid": "3" }}
+        />
+      </div>
+    );
+  }
+}
